28cd664080e4e70f2ddb32f243b98b726203a082
fujiwara <fujiwara@ml000.local>
2020-10-13 02:05:59 +0900

    save model

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import sys
+import os
+import csv
 import numpy as np
 import torch
 import torch.nn as nn
@@ -48,7 +50,7 @@ def main():
 
 
   # train
-  epochs = 2
+  epochs = 1
   for epoch in range(epochs):
     running_loss = 0.0
     for i, (inputs, labels) in enumerate(dataloader_train):
@@ -66,13 +68,40 @@ def main():
         remembered_running_loss = running_loss/batch_train
         running_loss = 0.0
 
+
+
     # remember accuracy for each epochs
     loss_for_visualize = np.append(loss_for_visualize,remembered_running_loss)
-    accuracy_for_visualize = np.append(accuracy_for_visualize, calculate_accuracy(dataloader_test,net))
+    remembered_accuracy = calculate_accuracy(dataloader_test,net)
+    accuracy_for_visualize = np.append(accuracy_for_visualize,remembered_accuracy)
     epochs_for_visualize = np.append(epochs_for_visualize, epoch+1)
 
 
+
+    # write record as a csv file for each epoch
+    file_path = './Exec_Record/'+ sys.argv[1]+'_'+sys.argv[2]+'_log.csv'
+    with open(file_path, 'a') as f:
+        writer = csv.writer(f, lineterminator='\n') # 改行コード（\n）を指定しておく
+        writer.writerow([epoch+1,remembered_accuracy,remembered_running_loss])
+
+
+    #10epochごとにmodelの保存をしておく
+    if(epoch % 10 == 1):
+      torch.save({
+        'epoch':epoch,
+        'model_state_dict':net.state_dict(),
+        'optimizer_state_dict':optimizer.state_dict(),
+      },'./Model_Record/'+sys.argv[1]+'_'+sys.argv[2]+'_' + str(epochs) + 'checkpoint.tar')
+
+
+  # save models after training
+  torch.save(net,'./Model_Record/'+sys.argv[1]+'_'+sys.argv[2]+'_' + str(epochs) + 'epochs.pth')
+
   print('Training Finished')
+
+
+
+
 
   # test
   print('Accuracy: {:2f} %%'.format(calculate_accuracy(dataloader_test,net)))
@@ -81,20 +110,20 @@ def main():
 
   # visualize
   plt.figure()
-  plt.title(sys.argv[1]+' '+sys.argv[2]+' acc')
+  plt.title(sys.argv[1]+'_'+sys.argv[2]+'_acc')
   plt.xlabel('epochs')
   plt.ylabel('accuracy')
   plt.ylim(0,100)
   plt.plot(epochs_for_visualize,accuracy_for_visualize)
-  plt.savefig(sys.argv[1]+' '+sys.argv[2]+' acc.png')
+  plt.savefig('./Visualize/' + sys.argv[1]+'_'+sys.argv[2]+'_acc.png')
 
 
   plt.figure()
-  plt.title(sys.argv[1]+' '+sys.argv[2]+' loss')
+  plt.title(sys.argv[1]+'_'+sys.argv[2]+'_loss')
   plt.xlabel('epochs')
   plt.ylabel('loss')
   plt.plot(epochs_for_visualize,loss_for_visualize)
-  plt.savefig(sys.argv[1]+' '+sys.argv[2]+' loss.png')
+  plt.savefig('./Visualize/' + sys.argv[1]+'_'+sys.argv[2]+'_loss.png')
 
 
 if __name__ == "__main__":
